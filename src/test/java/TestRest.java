1f17bf5754058e1b3f980d84db8e9c458e9b448c
TSConroe <1conroe1@gmail.com>
2018-05-29 21:11:13 +0300

    test

--- src/test/java/TestRest.java
+++ src/test/java/TestRest.java
@@ -1,21 +1,50 @@
 import static com.jayway.restassured.RestAssured.get;
+
+import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.response.Response;
+import com.jayway.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 import org.json.JSONArray;
 
-
+import java.util.Date;
 
 
 public class TestRest {
 
-    @Test
+    @Test (description = "GET")
     public void makeSureThatGoogleIsUp() {
         Response response = get("http://restcountries.eu/rest/v1/name/ukraine");
         JSONArray jsonResponse = new JSONArray(response.asString());
         String capital = jsonResponse.getJSONObject(0).getString("capital");
         Assert.assertEquals(capital, "Kiev");
 
+    }
+
+    @Test (description = "POST")
+    public void postRequestExampleTest() {
+        String someRandomString = String.format("%1$TH%1$TM%1$TS", new Date());
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("FirstName", someRandomString);
+
+
+        requestBody.put("LastName", someRandomString);
+        requestBody.put("UserName", someRandomString);
+        requestBody.put("Password", someRandomString);
+        requestBody.put("Email", someRandomString + "@gmail.com");
+
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+        request.body(requestBody.toString());
+        Response response = request.post("http://restapi.demoqa.com/customer/register");
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 201);
+        String successCode = response.jsonPath().get("SuccessCode");
+        Assert.assertEquals(successCode, "OPERATION_SUCCESS");
+        System.out.println(response.getBody().asString());
     }
 
 }
