6a41d1a0a4c5161e1f078a8c9a9e89d3b3dab5b0
TSConroe <1conroe1@gmail.com>
2018-06-18 23:14:36 +0300

    git api test

--- src/test/java/TestRest.java
+++ src/test/java/TestRest.java
@@ -1,19 +1,29 @@
 import static com.jayway.restassured.RestAssured.get;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.response.Response;
 import com.jayway.restassured.specification.RequestSpecification;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.JSONObject;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 import org.json.JSONArray;
 
+import java.io.IOException;
 import java.util.Date;
 
 
 public class TestRest {
 
-    @Test (description = "GET")
+    @Test(description = "GET")
     public void makeSureThatGoogleIsUp() {
         Response response = get("http://restcountries.eu/rest/v1/name/ukraine");
         JSONArray jsonResponse = new JSONArray(response.asString());
@@ -22,7 +32,7 @@ public class TestRest {
 
     }
 
-    @Test (description = "POST")
+    @Test(description = "POST")
     public void postRequestExampleTest() {
         String someRandomString = String.format("%1$TH%1$TM%1$TS", new Date());
 
@@ -47,4 +57,16 @@ public class TestRest {
         System.out.println(response.getBody().asString());
     }
 
+    @Test
+    public void givenUserDoesNotExists_whenUserInfoIsRetrieved_then404IsReceived()
+            throws ClientProtocolException, IOException {
+
+        HttpUriRequest request = new HttpGet("https://api.github.com/users/TSConroe");
+
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+
+        assertThat(
+                httpResponse.getStatusLine().getStatusCode(),
+                equalTo(HttpStatus.SC_OK));
+    }
 }
